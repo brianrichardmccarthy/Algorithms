b4119ad8f065c96c6dc4e4cb0607d7b3bd21a783
brianrichardmccarthy <brianmccarthy1@outlook.ie>
2016-09-26 10:17:13 +0100

    Testing the methods in pacemakerAPI

--- pacemaker-consoleLab06/src/controllers/Main.java
+++ pacemaker-consoleLab06/src/controllers/Main.java
@@ -50,9 +50,16 @@ public class Main {
 
 		pacemakerAPI.createUser("Bart", "Simpson", "bart@simpson.com", "secret");
 		pacemakerAPI.createUser("Homer", "Simpson", "homer@simpson.com", "secret");
-		pacemakerAPI.createUser("Lisa", "Simpson", "lisa@simpson.com", "secret");
+		pacemakerAPI.createUser("Lisa", "Simpson", " lisa@simpson.com", "secret");
 
 		Collection<User> users = pacemakerAPI.getUsers();
+		System.out.println(users);
+
+		User homer = pacemakerAPI.getUserByEmail("homer@simpson.com");
+		System.out.println(homer);
+
+		pacemakerAPI.deleteUser(homer.id);
+		users = pacemakerAPI.getUsers();
 		System.out.println(users);
 	}
 }
